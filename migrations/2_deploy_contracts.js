bf9dd31892984b86d1e2f80bf850351c99235d9a
Alejandro Munoz <amunoz797@gmail.com>
2018-02-23 18:38:43 -0500

    Added weiperspawn initialization

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ var Battle = artifacts.require("./Battle.sol");
 var KittyCore = artifacts.require("./CryptoKitties/KittyCore.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(NineLives).then(function() {
+  deployer.deploy(NineLives, 0).then(function() {
     return deployer.deploy(KittyCore).then(function() {
       return deployer.deploy(Battle, KittyCore.address).then(function() {
         return deployer.deploy(Arena, NineLives.address, Battle.address, KittyCore.address);
